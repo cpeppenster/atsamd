ce4bccfbfb9a0879cd94cdca9d108e2859cf4def
Christiaan <christiaan@hudl.com>
2023-08-01 13:46:05 +0200

    Fix UART flags

--- hal/src/sercom/uart.rs
+++ hal/src/sercom/uart.rs
@@ -404,11 +404,11 @@ mod charsize;
 pub use charsize::*;
 
 #[cfg(feature = "samd20")]
-#[path = "i2c/flags_samd20.rs"]
+#[path = "uart/flags_samd20.rs"]
 mod flags;
 
 #[cfg(not(feature = "samd20"))]
-#[path = "i2c/flags.rs"]
+#[path = "uart/flags.rs"]
 mod flags;
 
 pub use flags::*;
